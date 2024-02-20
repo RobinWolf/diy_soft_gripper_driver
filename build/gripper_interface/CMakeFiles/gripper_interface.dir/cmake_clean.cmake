33cb2da1f6d1760c086eac386ac22e0049c62dd2
Robin Wolf <woro1021@h-ka.de>
2024-02-20 14:53:49 +0100

    deleted build install and log folders

--- build/gripper_interface/CMakeFiles/gripper_interface.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gripper_interface"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gripper_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/gripper_interface/ament_cmake_python/gripper_interface/gripper_interface/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from gripper_interface.srv._gripper import Gripper  # noqa: F401
